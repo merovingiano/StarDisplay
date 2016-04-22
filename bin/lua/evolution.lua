450f97173a7246f4da4068fb0ea8fff79b2c6b37
robin.mills.27@gmail.com <whyrobinmills@hotmail.com>
2016-04-22 15:00:20 +0200

    evolution

---
+++ bin/lua/evolution.lua
@@ -0,0 +1,22 @@
+
+local evolution = {}
+
+function evolution.evolve_next()
+
+    return function()
+	   print("test yo")
+	   --while 1==1 do
+	   --end
+	end
+
+	--next generation
+	--sort on fitness criterium
+	-- duplicate first half to second half
+	-- random mutation
+	-- randomize random variables
+
+
+end
+
+
+return evolution
